798d7849c092a4729ba501ba323c2080788503e2
Robert <robertnostor@gmail.com>
2025-10-29 17:17:55 +0200

    Axes.cs, Grid.cs, Camer3DIsometrica.cs have been added

--- Mywindow.cs
+++ Mywindow.cs
@@ -9,7 +9,6 @@ namespace LearningOpenTK
 {
     public class Mywindow : GameWindow
     {
-        private Triangle triangle;
         public Mywindow(int width, int height, string title)
             : base(width, height, GraphicsMode.Default, title)
         {
@@ -19,9 +18,6 @@ namespace LearningOpenTK
         {
             base.OnLoad(e);
             GL.ClearColor(0.0f, 1.0f, 1.0f, 1.0f);
-
-            triangle = new Triangle();
-            triangle.Load();
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
@@ -29,8 +25,6 @@ namespace LearningOpenTK
             base.OnUpdateFrame(e);
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            triangle.Draw();
-            triangle.move(0.01f, 0.01f);
 
             SwapBuffers();
         }
